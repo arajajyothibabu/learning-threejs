f96b996bc17b42143ff0a59f60d8576ebadf755c
Jyothi Babu Araja <arajajyothibabu@apxor.com>
2017-09-16 18:12:02 +0530

    full screen renderer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@
  */
 import * as THREE from 'three';
 
+// Get the DOM element to attach to
+const container = document.querySelector('#container');
+
 // Set the scene size.
-const WIDTH = 400;
-const HEIGHT = 300;
+const WIDTH = container.clientWidth || 400;
+const HEIGHT = container.clientHeight || 300;
 
 // Set some camera attributes.
 const VIEW_ANGLE = 45;
@@ -13,21 +16,10 @@ const ASPECT = WIDTH / HEIGHT;
 const NEAR = 0.1;
 const FAR = 10000;
 
-// Get the DOM element to attach to
-const container =
-    document.querySelector('#container');
-
 // Create a WebGL renderer, camera
 // and a scene
 const renderer = new THREE.WebGLRenderer();
-const camera =
-    new THREE.PerspectiveCamera(
-        VIEW_ANGLE,
-        ASPECT,
-        NEAR,
-        FAR
-    );
-
+const camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
 const scene = new THREE.Scene();
 
 // Add the camera to the scene.
@@ -54,10 +46,7 @@ const sphereMaterial = new THREE.MeshLambertMaterial({
 // sphere geometry - we will cover
 // the sphereMaterial next!
 const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(
-        RADIUS,
-        SEGMENTS,
-        RINGS),
+    new THREE.SphereGeometry(RADIUS, SEGMENTS, RINGS),
     sphereMaterial
 );
 
@@ -69,8 +58,7 @@ sphere.position.z = -300;
 scene.add(sphere);
 
 // create a point light
-const pointLight =
-    new THREE.PointLight(0xFFFFFF);
+const pointLight = new THREE.PointLight(0xFFFFFF);
 
 // set its position
 pointLight.position.x = 10;
